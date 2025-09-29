b7b4a13d5bacb6bda2ad2c538e2c92f87df48cf5
finlaysr <60317020+finlaysr@users.noreply.github.com>
2025-09-29 15:17:46 +0100

    Finished day 2 part 2 in rust

--- Rust/day2/src/main.rs
+++ Rust/day2/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn main() {
     println!("Hello, world!");
 
-    let file_path = "src/input.txt";
+    let file_path = "/home/finlay/Code/AoC2024_2/inputs/day2.txt";
     let file = fs::read_to_string(file_path).expect("failed to read file");
 
     let data = file
@@ -17,21 +17,38 @@ fn main() {
 
     let mut total = 0;
     for line in data {
-        let mut valid = true;
-        let increasing = line[1] > line[0];
-        for x in 0..line.len() - 1 {
-            let diff = if increasing {
-                line[x + 1] - line[x]
-            } else {
-                line[x] - line[x + 1]
-            };
-            if !(diff >= 1 && diff <= 3) {
-                valid = false;
+        if is_valid(&line) {
+            total += 1;
+        } else {
+            let mut i = 0;
+            let mut done = false;
+            while i < line.len() && !done {
+                let mut temp = line.clone();
+                temp.remove(i);
+                done = is_valid(&temp);
+                if done {
+                    total += 1;
+                }
+                i += 1;
             }
         }
-        if valid {
-            total += 1;
+    }
+    println!("{total}");
+}
+
+fn is_valid(array: &Vec<isize>) -> bool {
+    let mut valid = true;
+    let increasing = array[1] > array[0];
+    for x in 0..array.len() - 1 {
+        let diff = if increasing {
+            array[x + 1] - array[x]
+        } else {
+            array[x] - array[x + 1]
+        };
+        if !(diff >= 1 && diff <= 3) {
+            valid = false;
         }
     }
-    println!("{total}")
+    valid
 }
+
